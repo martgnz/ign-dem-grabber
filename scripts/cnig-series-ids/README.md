0ac8aa87293a99cda78a8d2d0d1b69800a4dcd32
eskerda <eskerda@gmail.com>
2021-04-03 15:56:48 +0200

    add the laziest script to get cnig series ids
    sorry
    
                      ▒▒    ▒▒    ▒▒
                        ▒▒    ▒▒    ▒▒
                          ▓▓██▓▓██▓▓
                        ██▒▒▒▒▒▒▒▒▒▒██
                      ██▒▒▒▒░░▒▒░░▒▒▒▒██
                      ██▒▒▒▒░░▒▒░░▒▒░░██
                      ██▒▒▒▒░░▒▒░░▒▒░░██
                      ██▒▒▒▒░░▒▒░░▒▒░░██
            ████      ██▒▒▒▒░░▒▒░░▒▒░░██
          ██▒▒▒▒██    ██▒▒██░░▒▒░░██░░██
          ██▒▒▒▒██    ██▒▒▒▒░░▒▒░░▒▒░░██
          ██▒▒░░████████▒▒▒▒░░██░░▒▒░░████████████
          ██▒▒░░░░░░▒▒▒▒▒▒▒▒░░██░░▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒██
          ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░██░░▒▒░░▒▒▒▒░░░░░░▒▒██
            ████████████▒▒▒▒░░░░░░▒▒░░████████▒▒▒▒██
                      ██▒▒▒▒▒▒▒▒▒▒▒▒░░██    ██▒▒▒▒██
                      ██▒▒▒▒░░▒▒░░▒▒░░██    ██▒▒▒▒██
                      ██▒▒▒▒░░▒▒░░▒▒░░██      ████
                      ██▒▒▒▒░░▒▒░░▒▒░░██
                  ██████▒▒▒▒░░▒▒░░▒▒░░██
                ██▒▒▒▒▒▒▒▒▒▒░░▒▒░░▒▒░░██
              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
              ██▒▒▒▒██████████████▒▒░░██
              ██▒▒▒▒██          ██▒▒▒▒████████
              ██▒▒▒▒██          ██▒▒░░░░░░░░░░██
              ██▒▒▒▒██            ██▒▒▒▒▒▒▒▒▒▒██
              ██▒▒▒▒██              ██████████
                ████

---
+++ scripts/cnig-series-ids/README.md
@@ -0,0 +1,14 @@
+## Requirements
+
+This script requires lxml and curl on your system.
+
+```bash
+pip install lxml
+```
+
+## Usage
+
+```bash
+$ bash cnig.sh MDT02 > MDT02.csv
+$ bash cnig.sh MDT05 > MDT05.csv
+```
---
+++ scripts/cnig-series-ids/cnig.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# cnig-list MDT02 1
+function cnig-list {
+  local series=$1
+  local page=${2:-1}
+  inf "[+] Extracting $series, page $page"
+  # XXX this curl could suck less
+  curl 'http://centrodedescargas.cnig.es/CentroDescargas/resultadosArchivos' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0' -H 'Accept: text/html, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded;charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: http://centrodedescargas.cnig.es' -H 'Connection: keep-alive' -H 'Referer: http://centrodedescargas.cnig.es/CentroDescargas/index.jsp'  --data-raw 'geom=None&coords=%7B%22type%22+%3A+%22FeatureCollection%22%2C+%22features%22+%3A+%5B%7B%22type%22%3A%22Feature%22%2C%22geometry%22%3A%7B%22type%22%3A+%22Polygon%22%2C%22coordinates%22%3A+%5B%5B%5B-180%2C-90%5D%2C%5B-180%2C84.1640960027031%5D%2C%5B180.122131343973%2C84.1640960027031%5D%2C%5B180.122131343973%2C-90%5D%2C%5B-180%2C-90%5D%5D%5D%7D%7D%5D%7D&numPagina='$page'&numTotalReg=2248&codSerie='$series'&series='$series'&codProvAv=&codIneAv=&codComAv=&numHojaAv=&todaEsp=&todoMundo=&tipoBusqueda=VI&tipoArchivo=&contiene=&subSerieExt=&codSubSerie=&idProcShape=' 2> /dev/null
+}
+
+function main {
+  local series=${1:-"MDT02"}
+  local page=${2:-1}
+
+  echo "series,id,name,page"
+  while true; do
+    { cnig-list $series $page | grep -E 'secGeo|nombreGeo' | python -c "$(cat << EOF
+import sys
+import csv
+import lxml.html
+
+html = lxml.html.fromstring(sys.stdin.read())
+writer = csv.writer(sys.stdout)
+
+names = html.xpath('//input[contains(@id, "nombreGeo")]/@value')
+ids = html.xpath('//input[contains(@id, "secGeo")]/@value')
+assert(len(names) == len(ids))
+for _id, name in zip(names, ids):
+    writer.writerow(["$series", name, _id, "$page"])
+EOF
+    )" ; } || break
+    ((page+=1))
+    sleep 3
+  done
+}
+
+function err {
+  [[ $* =~ ^(\[.*\])(.*) ]] \
+    && >&2 echo -e "\033[1;31m${BASH_REMATCH[1]}\033[1;0m${BASH_REMATCH[2]}" \
+    || >&2 echo -e "$*"
+  exit 1
+}
+
+function inf {
+  [[ $* =~ ^(\[.*\])(.*) ]] \
+    && >&2 echo -e "\033[1;34m${BASH_REMATCH[1]}\033[1;0m${BASH_REMATCH[2]}" \
+    || >&2 echo -e "$*"
+}
+
+python -c 'import lxml.html' &> /dev/null || err "[!] lxml not found (pip install lxml)"
+
+main "$@"
